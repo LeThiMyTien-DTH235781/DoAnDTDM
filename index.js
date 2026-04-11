71425ef20aa821175e85a555d86a1a62e49585dc
MyTien <tien_dth235781@student.agu.edu.vn>
2026-04-12 00:38:23 +0700

    Up Bai Moi 7

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    secret: 'NoteCuteSecret',
     resave: false,
     saveUninitialized: true
 }));
